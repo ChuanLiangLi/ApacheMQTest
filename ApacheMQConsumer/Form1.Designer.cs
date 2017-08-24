055fec3cd7e3f8e6df686fbfc62ee8369d8f49fe
erbing <1536007949@qq.com>
2017-08-24 18:19:00 +0800

    创建消费者，生产者

---
+++ ApacheMQConsumer/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ApacheMQConsumer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblReceiveMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblReceiveMessage
+            // 
+            this.lblReceiveMessage.AutoSize = true;
+            this.lblReceiveMessage.Location = new System.Drawing.Point(137, 59);
+            this.lblReceiveMessage.Name = "lblReceiveMessage";
+            this.lblReceiveMessage.Size = new System.Drawing.Size(55, 15);
+            this.lblReceiveMessage.TabIndex = 0;
+            this.lblReceiveMessage.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 318);
+            this.Controls.Add(this.lblReceiveMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblReceiveMessage;
+    }
+}
+
---
+++ ApacheMQConsumer/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace ApacheMQConsumer
+{
+    public partial class Form1 : Form
+    {
+        static IMessageProducer prod1;
+        static ITextMessage message;
+        public Form1()
+        {
+            InitializeComponent();
+            InitConsumer();
+        }
+        public void InitConsumer()
+        {
+            //创建连接工厂
+            IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+            //通过工厂构建连接
+            IConnection connection = factory.CreateConnection();
+            //这个是连接的客户端名称标识
+            connection.ClientId = "firstQueueListener";
+            //启动连接，监听的话要主动启动连接
+            connection.Start();
+            //通过连接创建一个会话
+            ISession session = connection.CreateSession();
+            //通过会话创建一个消费者，这里就是Queue这种会话类型的监听参数设置
+            IMessageConsumer consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("firstQueue"), "filter1='demo1'");
+            //注册监听事件
+            consumer.Listener += new MessageListener(Consumer_Listener);
+
+            //2.      --------------创建生产者
+             //通过会话创建生产者，方法里面new出来的是MQ中的Queue
+             prod1 = session.CreateProducer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("returnQueue"));
+             //创建一个发送的消息对象
+            message = prod1.CreateTextMessage();
+            message.Properties.SetString("userId", "SC100410");
+        }
+
+        public void Consumer_Listener(IMessage message)
+        {
+            ITextMessage txtMsg = (ITextMessage)message;
+            lblReceiveMessage.Invoke(new DelegateRevMessage(RevMessage),txtMsg);
+            //lblReceiveMessage.Invoke((Action<ITextMessage>)((t) =>
+            //{
+            //    lblReceiveMessage.Text += string.Format(@"接收到:{0}{1}", t.Text, Environment.NewLine);
+            //}),txtMsg);
+        }
+       
+        public delegate void DelegateRevMessage(ITextMessage messag);
+        public void RevMessage(ITextMessage message)
+        {
+            lblReceiveMessage.Text += string.Format(@"接收到:{0}{1}",message.Text,Environment.NewLine);
+            DealMessge(message.Text);
+        }
+        public void DealMessge(string str)
+        {
+            //给这个对象赋实际的消息
+            message.Text = str + "{\"status\":\"success\"}" ;
+           
+            //生产者把消息发送出去，几个枚举参数MsgDeliveryMode是否长链，MsgPriority消息优先级别，发送最小单位，当然还有其他重载
+            prod1.Send(message, MsgDeliveryMode.NonPersistent, MsgPriority.Normal, TimeSpan.MinValue);
+        }
+
+    }
+}
